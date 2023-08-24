3d6861ea365570bb69f9c9962a85d668806e4d1c
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-24 13:46:34 -0500

    stub uup sidebar component

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SideBar: React.FC = () => {
+  return (
+    <div>
+      <h2>Nothing here yet but this is where the ingredients will be listed'
+      </h2>
+    </div>
+  )
+}
+
+export default SideBar
