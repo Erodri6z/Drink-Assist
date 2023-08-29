b62f23acf949a22db0fb8fb89ae9e4c1a2d8e4cf
Erodri6z <erikrodriguez2417@gmail.com>
2023-08-29 16:49:32 -0500

    add some styling to alphabet

--- src/components/Alphabet/Alphabet.tsx
+++ src/components/Alphabet/Alphabet.tsx
@@ -1,12 +1,13 @@
 import React from "react"
+import './Alphabet.css'
 
 const Alphabet: React.FC = () => {
 
   const alpha: string[] = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z'] 
   return(
-    <div>
+    <div className="alpha-div">
         { alpha.map((a) => 
-          <li>{a}</li>
+          <li className="alpha"><button className="a-btn">{a.toUpperCase()}</button></li>
         )}
     </div>
   )
