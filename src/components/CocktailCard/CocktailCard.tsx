f682edb89fd0cbdf1923c3a14076484835280417
Erodri6z <erikrodriguez2417@gmail.com>
2023-09-01 13:39:42 -0500

    stub up a cocktail card

---
+++ src/components/CocktailCard/CocktailCard.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const CocktailCard: React.FC = () => {
+
+
+  return (
+    <div>
+      <img src="" alt="placeholder" />
+      <div>
+        <h4 className="cocktail-name">Drink Name</h4>
+      </div>
+    </div>
+  )
+}
+
+export default CocktailCard
