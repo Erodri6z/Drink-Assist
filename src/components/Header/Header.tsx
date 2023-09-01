e0067584630bfdd55272ba5f36c9fbde30d1a791
Erodri6z <erikrodriguez2417@gmail.com>
2023-09-01 13:27:15 -0500

    add an image and a logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './Header.css'
-
+import GloomySpirits from "./../../assets/GloomySpirits.png"
 interface HeaderProps {
   title: string
 }
@@ -8,6 +8,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header>
+      <img src='GloomySpirits' alt="gloomySpirits" className="logo" srcSet={GloomySpirits} />
       <h1 className="title">{ title }</h1>
     </header>
   )
