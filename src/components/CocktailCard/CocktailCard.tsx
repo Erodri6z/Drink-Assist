aa741b30d0523622afdc91f5fa1b52327d323cc2
Erodri6z <erikrodriguez2417@gmail.com>
2023-09-02 16:46:36 -0500

    add styling to cocktail card

--- src/components/CocktailCard/CocktailCard.tsx
+++ src/components/CocktailCard/CocktailCard.tsx
@@ -1,5 +1,6 @@
 // import React from "react"
 import { Cocktail } from "../../types/Drinks/Drinks"
+import './CocktailCard.css'
 
 interface CocktailResultsProps {
   recipe: Cocktail
@@ -11,9 +12,9 @@ const CocktailCard = (props: CocktailResultsProps ) => {
   return (
     <div className="drink-card">
       <img src={props.recipe.strDrinkThumb} className="thumbnail" alt="thumbnail" />
-      <div className="drink-name-box">
+      {/* <div className="drink-name-box"> */}
         <h4 className="cocktail-name">{props.recipe.strDrink}</h4>
-      </div>
+      {/* </div> */}
     </div>
   )
 }
