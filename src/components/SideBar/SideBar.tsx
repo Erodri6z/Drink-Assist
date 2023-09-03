e1035d59c57c2d365fa8f74bff69c4efdb6b8a72
Erodri6z <erikrodriguez2417@gmail.com>
2023-09-03 11:58:33 -0500

    add functions to make search queries doable

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState } from "react"
 import * as drinkService from './../../services/drinkService'
 import './SideBar.css'
 
@@ -8,33 +9,67 @@ interface DataFetchingProps {
 
 const SideBar: React.FC<DataFetchingProps> = ({ setRecipe }) => {
 
-  // console.log(getAllDrinksByLetter('a'))
 
   const searchByIngredient = async (spirit: string) => {
     const drinksList = await drinkService.getDrinksByAlcohol(spirit)
     setRecipe(drinksList)
   }
 
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleSearch = (query: string) => {
+    searchByIngredient(query);
+  };
+
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSearch(searchQuery); 
+  };
+
   return (
     <>
     <div className="sidebar">
       <ul>
         <p>Spirits Or Ingredients</p>
-        <li><button onClick={() => searchByIngredient('vodka')}>Vodka</button></li>
-        <li><button onClick={() => searchByIngredient('whiskey')}>Whiskey</button></li>
-        <li><button onClick={() => searchByIngredient('scotch') }> Scotch</button></li>
-        <li><button onClick={() => searchByIngredient('gin') }> Gin </button></li>
-        <li><button onClick={() => searchByIngredient('tequila')} > Tequila </button></li>
-        <li><button onClick={() => searchByIngredient('mezcal')}>Mezcal</button></li>
-        <li><button onClick={() => searchByIngredient('light_rum')}>Light Rum</button></li>
-        <li><button onClick={() => searchByIngredient('dark_rum')}>Dark Rum</button></li>
-        <li><button onClick={() => searchByIngredient('cognac')}>Cognac</button></li>
-        <li><button onClick={() => searchByIngredient('brandy')}>Brandy</button></li>
-        <form>
-        <li>
-          <label htmlFor="spirit"></label>
-            <input type="text" className="i-search" placeholder="Other"/>
-            <button type="submit">Search</button>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('vodka')}>Vodka</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('whiskey')}>Whiskey</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('scotch') }> Scotch</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('gin') }>Gin</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('tequila')} > Tequila </button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('mezcal')}>Mezcal</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('light_rum')}>Light Rum</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('dark_rum')}>Dark Rum</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('cognac')}>Cognac</button>
+        </li>
+        <li className="spirit-list">
+          <button className="spirit-btn" onClick={() => searchByIngredient('brandy')}>Brandy</button>
+        </li>
+        <form onSubmit={handleSubmit}>
+          <li className="spirit-list">
+            <input type="text" value={searchQuery} onChange={handleChange} className="i-search" placeholder="Other"/>
+            <button type="submit" className="spirit-btn">Search</button>
           </li>
         </form>
       </ul>
