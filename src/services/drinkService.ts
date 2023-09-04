32b89dacc07723b551f2edb46484e36cd91bb206
Erodri6z <erikrodriguez2417@gmail.com>
2023-09-04 11:07:03 -0500

    refactor drinks service to  handle errors

--- src/services/drinkService.ts
+++ src/services/drinkService.ts
@@ -2,37 +2,43 @@ const URL = `${import.meta.env.VITE_BACK_END_SERVER_URl}/api/json/v1/1/`
 
 async function getAllDrinksByLetter(letter: string): Promise<any> {
   const apiUrl = `${URL}search.php?f=${letter}`
-  const res = await fetch(apiUrl, {
-    method: "GET",
-
-  })
-  return await res.json()
-
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error('Error picking a random')
+  }
+  const data = await res.json()
+  return data
 }
 
 
-async function getDrinksByAlcohol(spirit:string) {
+async function getDrinksByAlcohol(spirit:string): Promise<any> {
   const apiUrl = `${URL}filter.php?i=${spirit}`
-  const res = await fetch(apiUrl, {
-    method: 'GET'
-  })
-  return await res.json()
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error('Error picking a random')
+  }
+  const data = await res.json()
+  return data
 }
 
-async function searchByName(query: string) {
+async function searchByName(query: string): Promise<any> {
   const apiUrl = `${URL}search.php?s=${query}`
-  const res = await fetch(apiUrl, {
-    method: "GET"
-  })
-  return await res.json()
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error('Error picking a random')
+  }
+  const data = await res.json()
+  return data
 }
 
-async function randomPick() {
+async function randomPick(): Promise<any> {
   const apiUrl = `${URL}random.php`
-  const res = await fetch(apiUrl, {
-    method: "GET"
-  })
-  return await res.json()
+  const res = await fetch(apiUrl)
+  if (!res.ok) {
+    throw new Error('Error picking a random')
+  }
+  const data = await res.json()
+  return data
 }
 
 export {
