dfd5edf4c37d9d5f2b173d4ac01d23f47ef47ec5
Erodri6z <erikrodriguez2417@gmail.com>
2023-11-03 16:22:59 -0500

    stub up switch functionality

--- src/components/MulitpleSearch/MultiSearch.tsx
+++ src/components/MulitpleSearch/MultiSearch.tsx
@@ -1,91 +1,91 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import * as drinkService from '../../services/drinkService'
-// import './MultiSearch.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import * as drinkService from '../../services/drinkService'
+import './MultiSearch.css'
 
-// interface DataFetchingProps {
-//   setRecipe: React.Dispatch<React.SetStateAction<any>>
-// }
+interface DataFetchingProps {
+  setRecipe: React.Dispatch<React.SetStateAction<any>>
+}
 
-// const MultiSearch: React.FC<DataFetchingProps> = ({ setRecipe }) => {
+const MultiSearch: React.FC<DataFetchingProps> = ({ setRecipe }) => {
   
-//   const [ingredients, setIngredients] = useState<any[]>([])
-//   const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
+  const [ingredients, setIngredients] = useState<any[]>([])
+  const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
 
-//   useEffect(() => {
-//     const getIngredients = async () => {
-//       const ingredientList = await drinkService.getAlIngredients()
-//       setIngredients(ingredientList.drinks.sort((a:any , b:any) => a.strIngredient1.localeCompare(b.strIngredient1)))
-//     }
-//     getIngredients()
-//   }, [])
+  useEffect(() => {
+    const getIngredients = async () => {
+      const ingredientList = await drinkService.getAlIngredients()
+      setIngredients(ingredientList.drinks.sort((a:any , b:any) => a.strIngredient1.localeCompare(b.strIngredient1)))
+    }
+    getIngredients()
+  }, [])
 
-//   useEffect(() => {
-//     const intSearch = async () => {
-//       searchByIngredients()
-//     }
-//     intSearch()
-//   }, [selectedIngredients])
+  useEffect(() => {
+    const intSearch = async () => {
+      searchByIngredients()
+    }
+    intSearch()
+  }, [selectedIngredients])
 
-//   const searchByIngredients = async () => {
-//     if (selectedIngredients.length === 0){
-//       return
-//     }
-//     const drinksList = await drinkService.getDrinksByAlcohol(
-//       selectedIngredients.join(",")
-//       )
-//       setRecipe(drinksList)
-//     }
+  const searchByIngredients = async () => {
+    if (selectedIngredients.length === 0){
+      return
+    }
+    const drinksList = await drinkService.getDrinksByAlcohol(
+      selectedIngredients.join(",")
+      )
+      setRecipe(drinksList)
+    }
     
-//     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//       const selectedOption = e.target.value 
-//       if(selectedIngredients.includes(selectedOption)) {
-//         setSelectedIngredients((prevIngredient) => 
-//         prevIngredient.filter((ingredient) => ingredient !== selectedOption)
-//         )
-//       }else {
-//         setSelectedIngredients((prevIngredients) => [
-//           ...prevIngredients,
-//           selectedOption,
-//         ]
-//         )
-//       }
-//     }
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const selectedOption = e.target.value 
+      if(selectedIngredients.includes(selectedOption)) {
+        setSelectedIngredients((prevIngredient) => 
+        prevIngredient.filter((ingredient) => ingredient !== selectedOption)
+        )
+      }else {
+        setSelectedIngredients((prevIngredients) => [
+          ...prevIngredients,
+          selectedOption,
+        ]
+        )
+      }
+    }
 
-//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault()
-//     searchByIngredients()
-//   }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    searchByIngredients()
+  }
 
 
-//   const clearOptions = () => {
-//     setSelectedIngredients([])
-//   }
-//   return (
-//     <div className="i-search-component">
-//       <p className="multi">Multi Select</p>
-//     <form onSubmit={handleSubmit} >
-//       <select name="i-search" id="i-select" onChange={handleChange}>
-//         <option value="">Select Multi</option>
-//           {ingredients.map((i:any) => 
-//             <option key={i.strIngredient1} value={i.strIngredient1}>
-//               {i.strIngredient1}
-//             </option>
-//           )}
-//         </select>
-//       </form>
-//       <div className="ingredient-div">
-//         <ul className="ingredient-imgs">
-//           {selectedIngredients.map((i) =>
-//             <li key={i}>
-//               <img className="i-img" src={`https://www.thecocktaildb.com/images/ingredients/${i}-Small.png`} alt={i} />
-//             </li>
-//           )}
-//         </ ul>
-//       </div>
-//       <button onClick={clearOptions}>Clear</button>
-//     </div>
-//   )
-// }
+  const clearOptions = () => {
+    setSelectedIngredients([])
+  }
+  return (
+    <div className="i-search-component">
+      <p className="multi">Multi Select</p>
+    <form onSubmit={handleSubmit} >
+      <select name="i-search" id="i-select" onChange={handleChange}>
+        <option value="">Select Multi</option>
+          {ingredients.map((i:any) => 
+            <option key={i.strIngredient1} value={i.strIngredient1}>
+              {i.strIngredient1}
+            </option>
+          )}
+        </select>
+      </form>
+      <div className="ingredient-div">
+        <ul className="ingredient-imgs">
+          {selectedIngredients.map((i) =>
+            <li key={i}>
+              <img className="i-img" src={`https://www.thecocktaildb.com/images/ingredients/${i}-Small.png`} alt={i} />
+            </li>
+          )}
+        </ ul>
+      </div>
+      <button onClick={clearOptions}>Clear</button>
+    </div>
+  )
+}
 
-// // export default MultiSearch
+export default MultiSearch
--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,14 +1,44 @@
+import { Component } from 'react'
 import './Switch.css'
 
-const Switch = () => {
-  return (
-    <div>
+
+interface ToggleSwitchProps {
+  defaultChecked?: boolean
+}
+
+interface ToggleSwitchState {
+  isChecked: boolean
+}
+
+class Switch extends Component<ToggleSwitchProps, ToggleSwitchState> {
+  constructor(props: ToggleSwitchProps) {
+    super(props)
+    this.state = {
+      isChecked: props.defaultChecked || false
+    }
+  }
+
+  toggleComponent = () => {
+    this.setState((prevState) => ({
+      isChecked: !prevState.isChecked,
+    }))
+  }
+  
+  render() {
+    return (
+      <div>
       <label className="switch">
-        <input type="checkbox"/>
+        <input 
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.toggleComponent}
+          />
         <span className="slider"></span>
       </label>
     </div>
   )
 }
+}
+
 
 export default Switch
